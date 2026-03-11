f10bce3bfad0dc38a53258c8d8e7656db0ed4806
Antje Dittmer <antje.dittmer@dlr.de>
2026-03-11 17:40:06 +0100

    added correction factor input lambda_inf

--- Mathieusche Differentialgleichung/Programme/HelicopterConed_Flapping_2007.m
+++ Mathieusche Differentialgleichung/Programme/HelicopterConed_Flapping_2007.m
@@ -2,7 +2,7 @@
 clear; clc; close all;
 
 % Toggle: 1 = 6x6 Cyclic Analysis | 0 = 3x3 Coning Analysis
-isSixVec = 0; %[0,1];
+isSixVec = 0;
 pos0 = get(0,'defaultFigurePosition');
 
 for idxSix = 1:length(isSixVec)
@@ -14,7 +14,7 @@ for idxSix = 1:length(isSixVec)
     sigma    = 0.07;
     nu0beta     = 1.12;
     CT       = 0.005117;
-    lambda_i = sqrt(CT/2);
+    lambda_i_bar = sqrt(CT/2);
 
     % Using Option 1 for Inflow Constants
     KI = 16 / (pi * 45);
@@ -29,12 +29,23 @@ for idxSix = 1:length(isSixVec)
     g6 = gamma / 6;
     nuSq1 = nu0beta^2 - 1;
     C_inf = (Cl_alpha * sigma) / (16 * KI);
-    L_inf = (lambda_i / KI) + C_inf;
+    L_inf = (lambda_i_bar / KI) + C_inf;
 
     % Following your specific definition for the inflow terms
     C_inf_0 = (Cl_alpha * sigma) / (6 * Km);
     % The term is defined as negative for stability in the A matrix
-    L_inf_0 = -(1 / Km) * (4 * lambda_i + (Cl_alpha * sigma / 4));
+    L_inf_0 = -(1 / Km) * (4 * lambda_i_bar + (Cl_alpha * sigma / 4));
+
+
+%     a = 12/(sigma*Cl_alpha);
+%     b = 3/2;
+% 
+%     -b/(2*a) + sqrt( pi/180 - (b/(2*a))^2);
+% 
+% (6*CT/(sigma*Cl_alpha)+3*lambda_i_bar/2) *pi/180
+% 
+% 2/3 * Cl_alpha * sigma/(16 *lambda_i_bar + Cl_alpha*sigma)
+
 
     if isSix == 1
         A = [ -g8,    -2,    -nuSq1,  -g8,      0,     -g8;
@@ -70,7 +81,7 @@ for idxSix = 1:length(isSixVec)
         ev_dec = [eig(A_flap_only); eig(A_inf_only)];
     else
         % For 3x3 coning: rows 1-2 are flapping, row 3 is inflow
-        ev_dec = [eig(A(1:2, 1:2)); A(3,3)];
+        ev_dec = [eig(A(1:2, 1:2)); A(3,3)]; % Eventually change this?
     end
 
     % Custom Normalization:
@@ -84,6 +95,7 @@ for idxSix = 1:length(isSixVec)
 
     % Display Results to Command Window
     disp('Eigenvalues:'); disp(ev);
+    disp('Decoupled eigenvalues:'); disp(ev_dec);
     for idx = 1:size(V, 2)
         fprintf('\nMode %d Eigenvector (Mag | Phase Deg):\n', idx);
         disp([abs(V_norm(:,idx)), angle(V_norm(:,idx))*180/pi]);
@@ -146,7 +158,6 @@ for idxSix = 1:length(isSixVec)
     else
         % 3x3 coning
         u = ones(size(t))*deg2rad;
-
         sys_coned = ss(A, B, eye(3), 0);
         [y, t_out] = lsim(sys_coned, u, t);
     end
@@ -162,8 +173,8 @@ for idxSix = 1:length(isSixVec)
         % Construct the decoupled system for simulation
         sys_dec = ss(A_decoupled, B, eye(size(A, 1)), 0);
 
-        y1_dec = lsim(sys_dec, u1, t);
-        y2_dec = lsim(sys_dec, u2, t);
+        y_dec1 = lsim(sys_dec, u1, t);
+        y_dec2 = lsim(sys_dec, u2, t);
 
     else
         % 3x3 Coning Analysis: States [beta_dot; beta; lambda_i0]
@@ -172,49 +183,70 @@ for idxSix = 1:length(isSixVec)
         A_decoupled(3, 1:2) = 0;   % Remove flapping effect on inflow
         % Construct the decoupled system for simulation
         sys_dec = ss(A_decoupled, B, eye(size(A, 1)), 0);
-
         [y_dec] = lsim(sys_dec, u, t);
+        
+
+        % C_inf_0/(- L_inf_0) * pi/180 - y(end,3) % for debugging
+        lambda_inf = C_inf_0/(- L_inf_0) * pi/180;     
+        u_cor = u - g6* lambda_inf; %
+        [y_dec_cor] = lsim(sys_dec, u_cor, t);
     end
 
 
 
     %% 5. Plotting (States in Degrees)
+    lw = 1;
+    grey_color = 0.5*ones(1,3);
     if isSix == 1
-        % --- 6x6 Cyclic Results---
-        figure(2);
+        % --- 6x6 Cyclic Results: Flapping ---
+        % --- 6x6 Cyclic Results: Flapping ---
+
+        grey_color = 0.5*ones(1,3);
+        figure(2); clf;
         subplot(2,1,1);
-        plot(t, y1(:,3)*180/pi, 'b', t, y1(:,4)*180/pi, 'r--');
+        plot(t, y1(:,3)*180/pi, 'b'); hold on; % Coupled beta_1c [cite: 329]
+        plot(t, y1(:,4)*180/pi, 'r');          % Coupled beta_1s [cite: 329]
+        plot(t, y_dec1(:,3)*180/pi, 'k:', 'LineWidth', lw+0.5); % Decoupled beta_1c
+        plot(t, y_dec1(:,4)*180/pi, '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled beta_1s
         ylabel('Flapping [deg]'); title('Step Response: \theta_C = 1^\circ');
-        legend('\beta_{1C}', '\beta_{1S}'); grid on;
-        subplot(2,1,2);
-        plot(t, y2(:,3)*180/pi, 'b', t, y2(:,4)*180/pi, 'r--');
-        ylabel('Flapping [deg]'); xlabel('Time [s]');
-        title('Step Response: \theta_S = 1^\circ');
-        legend('\beta_{1C}', '\beta_{1S}'); grid on;
+        legend('Coupled \beta_{1C}', 'Coupled \beta_{1S}', 'Decoupled \beta_{1C}', 'Decoupled \beta_{1S}'); grid on;
 
-        % 6x6 Inflow plotting
-        figure(3);
+        subplot(2,1,2);
+        plot(t, y2(:,3)*180/pi, 'b'); hold on; % Coupled beta_1c [cite: 332]
+        plot(t, y2(:,4)*180/pi, 'r');          % Coupled beta_1s [cite: 332]
+        plot(t, y_dec2(:,3)*180/pi, 'k:', 'LineWidth', lw+0.5); % Decoupled beta_1c
+        plot(t, y_dec2(:,4)*180/pi, '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled beta_1s
+        ylabel('Flapping [deg]'); xlabel('Time [s]'); title('Step Response: \theta_S = 1^\circ');
+        legend('Coupled \beta_{1C}', 'Coupled \beta_{1S}', 'Decoupled \beta_{1C}', 'Decoupled \beta_{1S}'); grid on;
+
+        % --- 6x6 Cyclic Results: Inflow ---
+        figure(3); clf;
         subplot(2,1,1);
-        plot(t, y1(:,5)*180/pi, 'b', t, y1(:,6)*180/pi, 'r--');
+        plot(t, y1(:,5), 'b'); hold on; % Coupled lambda_1c [cite: 338]
+        plot(t, y1(:,6), 'r');          % Coupled lambda_1s [cite: 338]
+        plot(t, y_dec1(:,5), 'k:', 'LineWidth', lw+0.5); % Decoupled lambda_1c
+        plot(t, y_dec1(:,6), '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled lambda_1s
         ylabel('Inflow [-]'); title('Step Response: \theta_C = 1^\circ');
-        legend('\lambda_{1C}', '\lambda_{1S}'); grid on;
-        subplot(2,1,2);
-        plot(t, y2(:,5)*180/pi, 'b', t, y2(:,6)*180/pi, 'r--');
-        ylabel('Inflow [-]'); xlabel('Time [s]');
-        title('Step Response: \theta_S = 1^\circ');
-        legend('\lambda_{1C}', '\lambda_{1S}'); grid on;
+        legend('Coupled \lambda_{1C}', 'Coupled \lambda_{1S}', 'Decoupled \lambda_{1C}', 'Decoupled \lambda_{1S}'); grid on;
 
+        subplot(2,1,2);
+        plot(t, y2(:,5), 'b'); hold on; % Coupled lambda_1c [cite: 343]
+        plot(t, y2(:,6), 'r');          % Coupled lambda_1s [cite: 343]
+        plot(t, y_dec2(:,5), 'k:', 'LineWidth', lw+0.5); % Decoupled lambda_1c
+        plot(t, y_dec2(:,6), '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled lambda_1s
+        ylabel('Inflow [-]'); xlabel('Time [s]'); title('Step Response: \theta_S = 1^\circ');
+        legend('Coupled \lambda_{1C}', 'Coupled \lambda_{1S}', 'Decoupled \lambda_{1C}', 'Decoupled \lambda_{1S}'); grid on;
     else
         % --- 3x3 Coning Results - SINGLE PLOT MODE ---
-        lw = 1;
+
         figure(4); clf;
 
         subplot(2,1,1);
         plot(t_out, y(:,2)*180/pi, 'b', 'LineWidth', lw);  % beta_0 flapping
-
         hold on;
         plot(t_out, y_dec(:,2)*180/pi, 'k:','LineWidth', lw+0.5); % Decoupled flapping in dotted black
-        legend('Coupled', 'Decoupled');
+        plot(t_out, y_dec_cor(:,2)*180/pi, '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled flapping in dotted black
+        legend('Coupled', 'Decoupled', ['Decoupled', sprintf(' %2.2f deg', unique(u_cor)*180/pi)] );
         ylabel('\beta_0 [deg]');
         title('Step Response: \theta_0 = 1^\circ');
         grid on;
@@ -223,7 +255,8 @@ for idxSix = 1:length(isSixVec)
         plot(t_out, y(:,3), 'r', 'LineWidth', lw);         % lambda_i0 inflow
         hold on;
         plot(t_out, y_dec(:,3), 'k:','LineWidth', lw+ 0.5); % Decoupled flapping in dotted black
-        legend('Coupled', 'Decoupled');
+        plot(t_out, y_dec_cor(:,3), '--', 'Color', grey_color, 'LineWidth', lw+0.5); % Decoupled flapping in dotted black
+        legend('Coupled', 'Decoupled', ['Decoupled', sprintf(' %2.2f deg', unique(u_cor)*180/pi)] );
         ylabel('\lambda_{i0} [-]'); xlabel('Time [s]');
         grid on;
 
